54d8268c1b8c3a6b0a75180da71d0376cc39da90
Anatolie Molosag <anatoliemolosag@Anatolies-MacBook-Pro-2.local>
2024-02-18 23:05:14 -0600

    addingLoggers

--- src/main/java/com/example/springbootApp/customer/CustomerService.java
+++ src/main/java/com/example/springbootApp/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.example.springbootApp.customer;
 
 import com.example.springbootApp.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,8 @@ import java.util.List;
 @Service
 public class CustomerService {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
     private final CustomerRepository customerRepository;
 
     @Autowired             // Add parameter @Qualifier("fake") to fake data
@@ -18,12 +22,20 @@ public class CustomerService {
     }
 
     List<Customer> getCustomers(){
+        LOGGER.info("getCustomer was called");
         return customerRepository.findAll();
     }
 
-    Customer getCustomer(Long id){
-        return customerRepository.findById(id)
-                .orElseThrow( () -> new NotFoundException("customer with " + id + " not found"));
+    Customer getCustomer(Long id) {
+        return customerRepository
+                .findById(id)
+                .orElseThrow(
+                        () -> {
+                            NotFoundException notFoundException = new NotFoundException(
+                                    "customer with id " + id + " not found");
+                            LOGGER.error("error in getting customer {}", id, notFoundException);
+                            return notFoundException;
+                        });
     }
 
 
